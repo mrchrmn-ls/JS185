6d5693fcdaa75560630aacd549f02548534fb4f0
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-05-11 19:26:21 +0200

    implemented delete function

--- 02-interaction/expense.js
+++ 02-interaction/expense.js
@@ -15,45 +15,55 @@ class ExpenseData {
   }
 
   async list() {
-    await this.client.connect()
-                     .catch(error => logAndExit(error));
+    await this.client.connect().catch(error => logAndExit(error));
   
     let query = "SELECT * FROM expenses;"
-    let data = await this.client.query(query)
-                                .catch(error => logAndExit(error));
+    let data = await this.client.query(query).catch(error => logAndExit(error));
 
     this.printResults(data);
   
-    await this.client.end()
-                     .catch(error => logAndExit(error));
+    await this.client.end().catch(error => logAndExit(error));
   }
 
   async add(amount, memo) {
-    await this.client.connect()
-                     .catch(error => logAndExit(error));
+    await this.client.connect().catch(error => logAndExit(error));
   
     let queryText = "INSERT INTO expenses (amount, memo) VALUES ($1, $2);";
     let queryValues = [amount, memo];
-    await this.client.query(queryText, queryValues)
-                     .catch(error => logAndExit(error)).catch(error => logAndExit(error));
+    await this.client.query(queryText, queryValues).catch(error => logAndExit(error));
   
-    await this.client.end()
-                     .catch(error => logAndExit(error));
+    await this.client.end().catch(error => logAndExit(error));
   }
 
   async search(term) {
-    await this.client.connect()
-                     .catch(error => logAndExit(error));
+    await this.client.connect().catch(error => logAndExit(error));
 
-    let queryText = "SELECT * FROM expenses WHERE memo ILIKE $1";
+    let queryText = "SELECT * FROM expenses WHERE memo ILIKE $1;";
     let queryValues = [`%${term}%`];
-    let data = await this.client.query(queryText, queryValues)
-                                .catch(error => logAndExit(error)).catch(error => logAndExit(error));
+    let data = await this.client.query(queryText, queryValues).catch(error => logAndExit(error));
 
     this.printResults(data);
 
-    await this.client.end()
-                     .catch(error => logAndExit(error));
+    await this.client.end().catch(error => logAndExit(error));
+  }
+
+  async delete(id) {
+    await this.client.connect().catch(error => logAndExit(error));
+
+    let queryText = "SELECT * FROM expenses WHERE id = $1;";
+    let queryValues = [id];
+    let data = await this.client.query(queryText, queryValues).catch(error => logAndExit(error));
+
+    if (data.rowCount === 0) {
+      console.log(`There is no expense with the id '${id}'`);
+    } else {
+      let queryText = "DELETE FROM expenses WHERE id = $1;";
+      await this.client.query(queryText, queryValues).catch(error => logAndExit(error));
+      console.log("The following entry has been deleted:");
+      this.printResults(data);
+    }
+
+    await this.client.end().catch(error => logAndExit(error));
   }
 
   printResults(data) {
@@ -67,7 +77,7 @@ class ExpenseData {
       console.log(colArr.join(" | "));
     }
   
-    printHeader();
+//    printHeader();
     data.rows.forEach(row => {
       let columns = [
         String(row.id).padStart(3),
@@ -138,6 +148,16 @@ search QUERY - list expenses with a matching memo field
       }
       else {
         console.log("You must provide a search term.");
+      }
+
+    } else if (command === "delete") {
+      let id = this.args[3];
+
+      if (id) {
+        this.expenses.delete(id);
+      }
+      else {
+        console.log("You must provide an id.");
       }
     }
   }
