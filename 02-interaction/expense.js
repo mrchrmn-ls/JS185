a3ee26c8ef8f5a69f029b7b38856f066803ff78f
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-05-11 18:03:44 +0200

    after search function

--- 02-interaction/expense.js
+++ 02-interaction/expense.js
@@ -1,25 +1,10 @@
 const PROCESS = require('process');
 const { Client } = require("pg");
 
-
-class HELPERS {
-  static printHeader() {
-    console.log(
-  ` id |      date added |     amount | memo
-  -------------------------------------------------------------`
-    );
-  }
-
-  static printRow(colArr) {
-    console.log(colArr.join(" | "));
-  }
-
-  static logAndExit(error) {
-    console.log(error);
-    PROCESS.exit(1);
-  }
+function logAndExit(error) {
+  console.log(error);
+  PROCESS.exit(1);
 }
-
 
 class ExpenseData {
   constructor() {
@@ -31,14 +16,58 @@ class ExpenseData {
 
   async list() {
     await this.client.connect()
-                .catch(error => logAndExit(error));
+                     .catch(error => logAndExit(error));
   
     let query = "SELECT * FROM expenses;"
     let data = await this.client.query(query)
-                           .catch(error => logAndExit(error));
+                                .catch(error => logAndExit(error));
+
+    this.printResults(data);
   
-    HELPERS.printHeader();
+    await this.client.end()
+                     .catch(error => logAndExit(error));
+  }
+
+  async add(amount, memo) {
+    await this.client.connect()
+                     .catch(error => logAndExit(error));
   
+    let queryText = "INSERT INTO expenses (amount, memo) VALUES ($1, $2);";
+    let queryValues = [amount, memo];
+    await this.client.query(queryText, queryValues)
+                     .catch(error => logAndExit(error)).catch(error => logAndExit(error));
+  
+    await this.client.end()
+                     .catch(error => logAndExit(error));
+  }
+
+  async search(term) {
+    await this.client.connect()
+                     .catch(error => logAndExit(error));
+
+    let queryText = "SELECT * FROM expenses WHERE memo ILIKE $1";
+    let queryValues = [`%${term}%`];
+    let data = await this.client.query(queryText, queryValues)
+                                .catch(error => logAndExit(error)).catch(error => logAndExit(error));
+
+    this.printResults(data);
+
+    await this.client.end()
+                     .catch(error => logAndExit(error));
+  }
+
+  printResults(data) {
+    function printHeader() {
+      console.log(
+` id |      date added |     amount | memo
+-------------------------------------------------------------`);
+    }
+
+    function printRow(colArr) {
+      console.log(colArr.join(" | "));
+    }
+  
+    printHeader();
     data.rows.forEach(row => {
       let columns = [
         String(row.id).padStart(3),
@@ -46,25 +75,8 @@ class ExpenseData {
         row.amount.padStart(10),
         row.memo
       ];
-      HELPERS.printRow(columns);
+      printRow(columns);
     });
-  
-    await this.client.end()
-                .catch(error => logAndExit(error));
-  }
-  
-  
-  async add(amount, memo) {
-    await this.client.connect()
-                .catch(error => logAndExit(error));
-  
-    let queryText = `INSERT INTO expenses (amount, memo) VALUES ($1, $2);`;
-    let queryValues = [amount, memo];
-    await this.client.query(queryText, queryValues)
-                .catch(error => logAndExit(error)).catch(error => logAndExit(error));
-  
-    await this.client.end()
-                .catch(error => logAndExit(error));
   }
 }
 
@@ -114,9 +126,19 @@ search QUERY - list expenses with a matching memo field
       else {
         console.log("You must provide an amount and a memo.");
       }
-    } 
-    else if (command = "list") {
+
+    } else if (command === "list") {
       this.expenses.list();
+
+    } else if (command === "search") {
+      let term = this.args[3];
+
+      if (term) {
+        this.expenses.search(term);
+      }
+      else {
+        console.log("You must provide a search term.");
+      }
     }
   }
 }
